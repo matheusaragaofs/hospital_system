be9f9d8300a220ca792d2859ff263fb1c5255f9a
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-10 12:23:14 -0300

    Create a login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './home/home.component';
 import { PatientComponent } from './patient/patient.component';
 import { BookingComponent } from './booking/booking.component';
 import { ReportsComponent } from './reports/reports.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { ReportsComponent } from './reports/reports.component';
     HomeComponent,
     PatientComponent,
     BookingComponent,
-    ReportsComponent
+    ReportsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
