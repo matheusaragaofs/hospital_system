37442e1dac1b21b453e4cfd7f91e0f7fe32864f7
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-10 13:03:21 -0300

    Bind route guard in safe routes in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { PatientComponent } from './patient/patient.component';
 import { BookingComponent } from './booking/booking.component';
 import { ReportsComponent } from './reports/reports.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -14,19 +15,24 @@ const routes: Routes = [
   },
   {
     path: 'patient',
-    component: PatientComponent
+    component: PatientComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'booking',
-    component: BookingComponent
+    component: BookingComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'reports',
-    component: ReportsComponent
+    component: ReportsComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [AuthGuard]
+
   }
 ];
 
