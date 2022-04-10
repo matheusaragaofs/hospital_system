60392552866ed4b416bfe8565fe9fbd9ed3ddce3
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-10 12:11:15 -0300

    Create a home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,11 +19,12 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { HomeComponent } from './home/home.component';
+import { PatientsComponent } from './patients/patients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
