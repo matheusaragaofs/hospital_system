6927d70460aad45fbe4d874b3bb78dd3c03d815e
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-10 12:23:54 -0300

    Bind a login route in app-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { PatientComponent } from './patient/patient.component';
 import { BookingComponent } from './booking/booking.component';
 import { ReportsComponent } from './reports/reports.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path: 'reports',
     component: ReportsComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
