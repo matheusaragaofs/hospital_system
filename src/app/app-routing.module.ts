9756cf98dcf865ecf3fb185816aead21e74d66f8
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-10 12:22:09 -0300

    Bind routes in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PatientComponent } from './patient/patient.component';
+import { BookingComponent } from './booking/booking.component';
+import { ReportsComponent } from './reports/reports.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'patient',
+    component: PatientComponent
+  },
+  {
+    path: 'booking',
+    component: BookingComponent
+  },
+  {
+    path: 'reports',
+    component: ReportsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
