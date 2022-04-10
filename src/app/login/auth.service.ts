c136c2683bd013f7050ee1c39185e695a7d88422
Lucas Leonardo <lucas.leonardo@g.globo>
2022-04-10 12:47:30 -0300

    Create login and logout funcionality in authservice

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,9 +1,36 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public isAuthenticated: boolean = false;
+  public authEmitter = new EventEmitter<boolean>()
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  login(email: string, password: string): boolean {
+    if (email === "lukita" && password === "111") {
+      this.isAuthenticated = true;
+      this.authEmitter.emit(true);
+      this.router.navigate(['/']);
+
+      return true
+    } else {
+      this.isAuthenticated = false;
+      this.authEmitter.emit(false);
+
+      return false
+    };
+  }
+
+  get getIsAuthenticated() {
+    return this.isAuthenticated;
+  }
+
+  logout() {
+    this.authEmitter.emit(false)
+    this.router.navigate(['/login'])
+  }
 }
