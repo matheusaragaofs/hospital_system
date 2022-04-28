3b69f8de3e869643d06d3ffa82b3e757f9dcf418
Wesley Alves <was4@cin.ufpe.br>
2022-04-27 21:10:34 -0300

    Create first version of report scenario

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,4 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexTitleSubtitle,
+  ApexXAxis,
+  ApexFill
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  title: ApexTitleSubtitle;
+};
 
 @Component({
   selector: 'app-reports',
@@ -6,10 +29,114 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports.component.sass']
 })
 export class ReportsComponent implements OnInit {
+  @ViewChild("chart") chart:any = ChartComponent;
+  public chartOptions!: Partial<ChartOptions> | any;
+  public selectSecondDoctor: boolean = false;
 
-  constructor() { }
+  toggleCompareDoctor(): void {
+    this.selectSecondDoctor = !this.selectSecondDoctor
+  }
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Média de pacientes atendidos",
+          data: [2.3, 3.1, 4.0, 5, 4.0, 3.6, 3.2]
+        }
+      ],
+      chart: {
+        height: "100%",
+        width: "98%",
+        type: "bar",
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            position: "top" // top, center, bottom
+          },
+        }
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function(val:any) {
+          return val
+        },
+        offsetY: -20,
+        style: {
+          fontSize: "12px",
+          colors: ["#304758"]
+        }
+      },
+
+      xaxis: {
+        categories: [
+          "Seg",
+          "Ter",
+          "Qua",
+          "Qui",
+          "Sex",
+          "Sab",
+          "Dom"
+        ],
+        position: "top",
+        labels: {
+          offsetY: -18
+        },
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        crosshairs: {
+          fill: {
+            type: "gradient",
+            gradient: {
+              colorFrom: "#D8E3F0",
+              colorTo: "#BED1E6",
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5
+            }
+          }
+        },
+        tooltip: {
+          enabled: true,
+          offsetY: -35
+        }
+      },
+      fill: {
+        colors:['#303f9f'],        
+      },
+      yaxis: {
+        axisBorder: {
+          show: false
+        },
+        axisTicks: {
+          show: false
+        },
+        labels: {
+          show: false,
+          formatter: function(val:any) {
+            return val + "%";
+          }
+        }
+      },
+      title: {
+        text: "Média de pacientes atendidos por dia",
+        floating: false,
+        offsetY: 530,
+        align: "center",
+        style: {
+          color: "#444"
+        }
+      }
+    };
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+
