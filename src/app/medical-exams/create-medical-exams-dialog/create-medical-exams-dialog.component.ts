e8bc581123d053d57f0cecf4eddbf0f26ed8040b
Matheus <matheusaragaofs@gmail.com>
2022-05-02 18:03:36 -0300

    change the logic to pick an doctor by exam

--- src/app/medical-exams/create-medical-exams-dialog/create-medical-exams-dialog.component.ts
+++ src/app/medical-exams/create-medical-exams-dialog/create-medical-exams-dialog.component.ts
@@ -12,7 +12,7 @@ export class CreateMedicalExamsDialogComponent implements OnInit {
   public selectedExamId: string = '';
   public selectedDoctorId: string = '';
   public scheduledDate: string = '';
-  public patientName: string = ''
+  public patientName: string = '';
   public patientFound: boolean = false;
   public exams: { label: string; id: string }[] = [
     {
@@ -32,85 +32,82 @@ export class CreateMedicalExamsDialogComponent implements OnInit {
       id: 'electrocardiogram',
     },
   ];
-  public doctors2 = { 
+  public doctors: { [key: string]: { label: string; id: number }[] } = {
     x_ray: [
-    {
-      label: 'Dra. Tatiana Noronha',
-      id: 1,
-    },
-    {
-      label: 'Dr. Jair Malta ',
-      id: 2,
-    },
-    {
-      label: 'Dra. Celeste Bessa ',
-      id: 3,
-    },
-  ],
-    tomography:[
       {
-      label: 'Dra. Priscila Almeida',
-      id: 4,
-       },
+        label: 'Dra. Tatiana Noronha',
+        id: 1,
+      },
       {
-      label: 'Dr. Denilson  Abelho ',
-      id: 5,
-       },
+        label: 'Dr. Jair Malta ',
+        id: 2,
+      },
       {
-      label: 'Dra. Emily  Zagalo',
-      id: 6,
-       }
-  ],
-  blood_count:[
-    {
-      label: 'Dra. Céline Ramires ',
-      id: 7,
-    },
-    {
-      label: 'Dra. Ana Catarina ',
-      id: 8,
-    }
-  ]
-  }
-  
-  
-  
-  
-  
-  public doctors: { label: string; id: string }[] = [
-    {
-      label: 'Dr. Miguel Arraes',
-      id: '1212121',
-    },
-    {
-      label: 'Dra. Priscila Senna',
-      id: '21313123',
-    },
-    {
-      label: 'Dra. Raphaela sANTOS',
-      id: '123123123',
-    },
-    {
-      label: 'Dr. Irineu',
-      id: '1231231',
-    },
-  ];
+        label: 'Dra. Celeste Bessa ',
+        id: 3,
+      },
+    ],
+    tomography: [
+      {
+        label: 'Dra. Gina Cedraz',
+        id: 4,
+      },
+      {
+        label: 'Dr. Marcelino  Abelho ',
+        id: 5,
+      },
+      {
+        label: 'Dra. Emily  Zagalo',
+        id: 6,
+      },
+    ],
+    blood_count: [
+      {
+        label: 'Dra. Céline Ramires ',
+        id: 7,
+      },
+      {
+        label: 'Dra. Ana Catarina ',
+        id: 8,
+      },
+      {
+        label: 'Dra. Branca Marques ',
+        id: 9
+      },
+    ],
+    electrocardiogram: [
+      {
+        label: 'Taíssa Naves ',
+        id: 10,
+      },
+      {
+        label: 'Stella Remígio ',
+        id: 11,
+      },
+      {
+        label: 'Renata Lameiras',
+        id: 12,
+      },
+    ],
+  };
+
   public cpf: string = '';
   onSubmit(): void {
-    const { scheduledDate, selectedExamId, patientName, selectedDoctorId} = this;
-  
+    const { scheduledDate, selectedExamId, patientName, selectedDoctorId } =
+      this;
+
     const registerData = {
-     patientName,
-     selectedExamId,
-     selectedDoctorId,
-     scheduledDate
+      patientName,
+      selectedExamId,
+      selectedDoctorId,
+      scheduledDate,
     };
 
     console.log(registerData);
   }
 
   togglePatientFound() {
-    this.patientFound = !this.patientFound
+    this.patientFound = !this.patientFound;
   }
 
   setSelectedExam(event: MatSelectChange): void {
