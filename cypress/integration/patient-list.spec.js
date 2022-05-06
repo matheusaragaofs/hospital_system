55d20f97badf8878617f2b025df08cfcb5c5e1b7
Wesley Alves <was4@cin.ufpe.br>
2022-05-05 23:45:47 -0300

    Improve patient list tests

--- cypress/integration/patient-list.spec.js
+++ cypress/integration/patient-list.spec.js
@@ -19,7 +19,29 @@ describe("Testes da tela de lista de pacientes", () => {
         cy.get("input[name=cpf]").type("13051905878");
         cy.get("input[name=phone_number]").type("81984258970");
         cy.get('mat-select').click().get('mat-option').contains('Masculino').click();
-
+        cy.get("mat-datepicker-toggle").click();
+        cy.get('button').contains('1').click();
+        cy.get("input[name=address]").type("Rua Sport Club do Recife - Ilha do Retiro, Recife - PE");
+        cy.get("input[name=cep").type("50750560");
+        cy.get('[name="btn-confirm"]').click();
+        cy.get("[data-error]").should("not.exist");
     });
+
+
+    it("Deletar um paciente deve fazer ele sumir da lista", () => {
+        cy.get('[name="btn-add-patient"]').click();
+        cy.get("input[name=name]").type("Joaozinho");
+        cy.get("input[name=cpf]").type("14498235812");
+        cy.get("input[name=phone_number]").type("81984258970");
+        cy.get('mat-select').click().get('mat-option').contains('Masculino').click();
+        cy.get("mat-datepicker-toggle").click();
+        cy.get('button').contains('4').click();
+        cy.get("input[name=address]").type("Rua Antonio Aureliano");
+        cy.get("input[name=cep").type("50730150");
+        cy.get('[name="btn-confirm"]').click();       
+    });
+
+    
+
 
   });
