3ccda348e1fd101f6de5378c7d55d4a8963f2e45
Wesley Alves <was4@cin.ufpe.br>
2022-05-06 23:08:19 -0300

    Add medical exams tests

--- cypress/integration/exams.spec.js
+++ cypress/integration/exams.spec.js
@@ -9,6 +9,58 @@ describe("Testes da tela de exames médicos", () => {
     });
   
     it("Clicar em Novo paciente deve gerar a exibição de um modal", () => {
-        cy.get('[name="btn-add-patient"]').click();
+        cy.get('[data-cy=new-exam-btn]').click();
+        cy.get('#mat-dialog-0').should('be.visible');
     });
+
+    /*
+    it("Cadastrar um novo exame não deve ser possível sem todos os campos preenchidos", () => {
+        cy.get('[data-cy=new-exam-btn]').click();
+        cy.get("input[name=cpf]").type("13051905878");
+        cy.get('[data-cy=search-btn]').click();
+        cy.get('[data-cy=submit-btn]').should('be.disabled');
+    });
+    */
+
+    it("Cadastrar um novo exame corretamente não deve gerar erros", () => {
+        cy.get('[data-cy=new-exam-btn]').click();
+        cy.get("input[name=cpf]").type("13051905878");
+        cy.get('[data-cy=search-btn]').click();
+        cy.get('[data-cy=exam-select]').click().get('mat-option').contains('Tomografia').click();
+        cy.get('[data-cy=doctor-select]').click().get('mat-option').contains('Abelho').click();
+        cy.get("mat-datepicker-toggle").click();
+        cy.get('.actions > .mat-focus-indicator > .mat-button-wrapper > .mat-icon').click();
+        cy.get('[data-cy=submit-btn]').click();
+        cy.get("[data-error]").should("not.exist");
+    });
+
+    it("Visualizar um exame deve abrir uma dialog box com informações sobre ele ", () => {
+        cy.get('[data-cy=acoes]').last().click({force: true}).as('actionsBtn');
+
+        cy.get('@actionsBtn').should('be.visible');
+        cy.get('@actionsBtn').click({ force: true });
+
+        cy.get('[data-cy=view]').last().click({force: true});
+
+        cy.get('#mat-dialog-0').should("be.visible");
+
+        cy.get("[data-error]").should("not.exist");
+    }); 
+
+    it("Deletar um exame deve removê-lo da lista", () => {
+        cy.get('[data-cy=acoes]').last().click({force: true}).as('actionsBtn');
+
+        cy.get('@actionsBtn').should('be.visible');
+        cy.get('@actionsBtn').click({ force: true });
+
+        cy.get('[data-cy=delete]').last().click({force: true});
+
+
+        cy.get('.footer-buttons > .mat-primary').click();
+
+
+        cy.get("[data-error]").should("not.exist");
+
+
+    }); 
   });
