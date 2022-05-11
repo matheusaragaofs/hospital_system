79cecebf5d02ae0af6626b90db23bc1f2454ff76
Lucas Leonardo <lucas.leonardo@g.globo>
2022-05-11 10:17:30 -0300

    Add a tests to exam room reservation

--- cypress/integration/exams.spec.js
+++ cypress/integration/exams.spec.js
@@ -33,11 +33,41 @@ describe("Testes da tela de exames médicos", () => {
         cy.get('[data-cy=search-btn]').click();
         cy.get('[data-cy=exam-select]').click().get('mat-option').contains('Tomografia').click();
         cy.get('[data-cy=doctor-select]').click().get('mat-option').contains('Abelho').click();
+        cy.get('[data-cy=exam-room]').click().get('mat-option').contains('D1').click();
         cy.get("mat-datepicker-toggle").click();
-        cy.get('.actions > .mat-focus-indicator > .mat-button-wrapper > .mat-icon').click();
+        cy.get('button').contains('19').click();
+        cy.get('[data-cy=exam-hour]').click().get('mat-option').contains('9:30').click();
         cy.get('[data-cy=submit-btn]').click();
+        
         cy.get("[data-error]").should("not.exist");
     });
+
+    it("Cadastrar um novo exame em uma sala e horário ja reservados náo deve ser possível", () => {
+        cy.get('[data-cy=new-exam-btn]').click();
+        cy.get("input[name=cpf]").type("11111111111");
+        cy.get('[data-cy=search-btn]').click();
+        cy.get('[data-cy=exam-select]').click().get('mat-option').contains('Tomografia').click();
+        cy.get('[data-cy=doctor-select]').click().get('mat-option').contains('Abelho').click();
+        cy.get('[data-cy=exam-room]').click().get('mat-option').contains('D1').click();
+        cy.get("mat-datepicker-toggle").click();
+        cy.get('button').contains('19').click();
+        cy.get('[data-cy=exam-hour]').click().get('mat-option').contains('10:00').click();
+        cy.get('[data-cy=submit-btn]').click();
+        cy.get('body').click(0,0);
+
+
+        cy.get('[data-cy=new-exam-btn]').click();
+        cy.get("input[name=cpf]").type("22222222222");
+        cy.get('[data-cy=search-btn]').click();
+        cy.get('[data-cy=exam-select]').click().get('mat-option').contains('Tomografia').click();
+        cy.get('[data-cy=doctor-select]').click().get('mat-option').contains('Abelho').click();
+        cy.get('[data-cy=exam-room]').click().get('mat-option').contains('D1').click();
+        cy.get("mat-datepicker-toggle").click();
+        cy.get('button').contains('19').click();
+        cy.get('[data-cy=exam-hour]').click().get('mat-option').contains('10:00');
+        cy.get('[data-cy=submit-btn]').should('be.disabled');
+    });
+
 
     it("Visualizar um exame deve abrir uma dialog box com informações sobre ele ", () => {
         cy.get('[data-cy=acoes]').last().click({force: true}).as('actionsBtn');
